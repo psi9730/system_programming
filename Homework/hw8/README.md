28ff2be7d02c4e49bd80692d80b6c70f704e6e1e
psi9730 <psi9730@naver.com>
2018-11-11 20:37:07 +0900

    add q5, q6 makefile

---
+++ Homework/hw8/README.md
@@ -0,0 +1,16 @@
+q5
+=======
+```
+	$ make
+	$ ./q5 hello output.txt
+	$ cat output.txt
+
+```
+
+q6
+======
+```
+	$ make
+	$ ./q6
+
+```
--- Homework/hw8/q5/q5.c
+++ Homework/hw8/q5/q5.c
@@ -7,9 +7,6 @@
 int main(int argc, char *argv[])
 {
   int status;
-  char *hello = "Hello, System Programming!";
-  printf("Input string to send to child : %s\n", hello);
-  // open pipe
   int p[2];
   if (pipe(p) == -1) {
     perror("Cannot open pipe");
@@ -18,31 +15,39 @@ int main(int argc, char *argv[])
 
   // fork
   pid_t pid = fork();
-  if (pid > 0) {
+  if (pid == 0) {
     // parent
-    
+    char *child_argv[]={
+        argv[1],
+        NULL,
+        NULL
+    };
     // redirect write end of pipe to STDOUT
-    printf("Parent pid = %d: sending to child %d : %s\n", getpid(), pid, hello);
     if (dup2(p[1], STDOUT_FILENO) == -1) {
       perror("Cannot redirect stdout");
       return 1;
     }
     // close unused read end
     close(p[0]);
-    printf("%s",hello);
-    sleep(1);
-  } else if (pid == 0) {
+    execv(child_argv[0],child_argv);
+    perror("Cannot execute child.");
+  } else if (pid > 0) {
     int i, res;
     FILE *pipe_read;
-    pid_t parentPID=getppid();
+    FILE *fp;
+    sleep(1);
     // convert file descriptor into file stream
     pipe_read = fdopen(p[0], "r");
     // close unused write end of pipe
     close(p[1]);
-    char *temp = malloc(40*sizeof(char));
+    char temp[40]={NULL};
     // read numbers and print as hex until pipe closed
     fread(temp, sizeof(char),40, pipe_read);
-    printf("Child pid = %d, received from parent %d : %s\n",getpid(), parentPID, temp);
+    fp= fopen(argv[2],"w");
+    if(fp!=NULL){
+	fflush(stdout);
+        fwrite(temp, sizeof(char), sizeof(temp), fp);
+    }
   }
 
   return 0;
